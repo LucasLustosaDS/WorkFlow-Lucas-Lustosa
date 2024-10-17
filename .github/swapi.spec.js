c650ef662c5bedfee7603dd5aa7602344313564e
LucasLustosaDS <131633254+LucasLustosaDS@users.noreply.github.com>
2024-10-17 10:44:17 -0300

    Add files via upload

---
+++ .github/swapi.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest') 
+
+test('Deve visualizar informações de cadastro, quando buscar por uma pessoa existente', async ()=> {
+    // https://swapi.dev/api
+    // /people/1
+    const resposta = await request('https://swapi.dev/api').get('/people/10');
+
+    // Verificar se o status da requisição está retornando com status 200
+    expect(resposta.status).toBe(200);
+    // Verificando a garantia de essas informações existem, não sendo indefinida
+    expect(resposta.body.films).toBeDefined();
+    // Verificando se recupera no corpo de conteúdo, um ou mais veículos (aeronaves)
+    expect(resposta.body.vehicles.length).toBeGreaterThan(0);
+    // Verificando se recupera um conteúdo específico, por exemplo o nome da primeira pessoa
+    expect(resposta.body.name).toBe('Obi-Wan Kenobi');
+
+});
+
+test('Deve receber uma mensagem de erro, quando buscar por uma pessoa inexistente', async () => {
+    const resposta = await request('https://swapi.dev/api').get('/people/9999');
+    // Verifica se o status da requisição está retornando falso com status 400
+    expect(resposta.status).toBe(404);
+    // Verifica o valor do corpo vazio não encontrado
+    expect(resposta.body.detail).toBe('Not found');
+    // Podemos verificar também o corpo vazio como objeto
+    expect(resposta.body).toMatchObject({
+        detail: 'Not found'
+    });
+
+});
+
